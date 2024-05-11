1d6052f62290c1e6b6e6819935ff08bd2411356d
Dmitry Logvinenko <dmlogv@gmail.com>
2024-05-12 01:11:19 +0300

    Add some useful chars under the index thumbs

--- keyboards/jorne/keymaps/dmlogv/keymap.c
+++ keyboards/jorne/keymaps/dmlogv/keymap.c
@@ -128,10 +128,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT(
-      _______, KC_UNDS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  F12_RGU,
-               PLS_LCT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, MIN_RCT,
-               EQL_LAL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-                                          _______, _______, _______,      ESC_LWR, BSP_RSH, DEL_RSE
+      _______, KC_UNDS, KC_F1,   KC_F2,   KC_F3,   KC_UNDS, KC_LBRC,      KC_RBRC,  KC_EQL,   KC_F8,   KC_F9,  KC_F10,  KC_F11, F12_RGU,
+               PLS_LCT, KC_EXLM, KC_AT,   KC_HASH, KC_MINS, KC_LPRN,      KC_RPRN, KC_PLUS, KC_ASTR, KC_LPRN, KC_RPRN, MIN_RCT,
+               EQL_LAL, KC_1,    KC_2,    KC_3,    KC_SCLN, KC_LCBR,      KC_RCBR, KC_COLN,    KC_8,    KC_9,    KC_0, _______,
+                                          _______, _______, _______,      ENT_LWR, BSP_RSH, DEL_RSE
     ),
 
     [_RAISE] = LAYOUT(
